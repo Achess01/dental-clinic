eed04143edbc6cdbcaf5e511010256d14313398c
Achess <jealtzo26@gmail.com>
2022-08-14 23:45:43 -0600

    Applying migrations. Registering models for admin site

--- api/attention/admin.py
+++ api/attention/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from .models import (
+    Appointment,
+    Diagnostic,
+    Treatment,
+    TreatmentPerformed,
+    Patient,
+    Record,
+    NoAttendedRecord,
+    LateRecord
+)
+
+admin.site.register(Appointment)
+admin.site.register(Diagnostic)
+admin.site.register(Treatment)
+admin.site.register(TreatmentPerformed)
+admin.site.register(Patient)
+admin.site.register(Record)
+admin.site.register(NoAttendedRecord)
+admin.site.register(LateRecord)
--- api/attention/models/__init__.py
+++ api/attention/models/__init__.py
@@ -2,3 +2,4 @@ from .patients import *
 from .appointments import *
 from .diagnostics import *
 from .records import *
+from .more_records import *
