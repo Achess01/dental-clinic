39130c75c764b900425216eda9848ce6a8e81f54
Achess <jealtzo26@gmail.com>
2022-08-20 15:09:30 -0600

    Endpoints to retrieve choices for record fields

--- api/attention/serializers/records.py
+++ api/attention/serializers/records.py
@@ -1,7 +1,9 @@
 """ Records serializers """
 
+# Django
+from django.utils import timezone
+
 # Django REST Framework
-from dataclasses import fields
 from rest_framework import serializers
 
 # Models
@@ -38,15 +40,19 @@ class RecordModelSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         attendance_state = 'attendance_state'
+        diagnostic = 'diagnostic'
         if attendance_state in validated_data and instance.attendance_state == '':
             fields = RecordCreateModelSerializer(instance).data
-            fields['appointment'] = instance.appointment            
+            fields['appointment'] = instance.appointment
             if validated_data[attendance_state] == 'L' and not self.is_saved(instance.pk, LateRecord):
                 LateRecord.objects.create(**fields)
             elif validated_data[attendance_state] == 'N' and not self.is_saved(instance.pk, NoAttendedRecord):
                 NoAttendedRecord.objects.create(**fields)
+
+        if diagnostic in validated_data:
+            instance.diagnostic_date = timezone.now()
+
         return super().update(instance, validated_data)
-    
 
     def is_saved(self, pk, model):
         return model.objects.filter(pk=pk).exists()
--- api/attention/views/records.py
+++ api/attention/views/records.py
@@ -2,6 +2,8 @@
 
 # Django REST Framework
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Serializers
 from attention.serializers import (
@@ -26,6 +28,7 @@ from users.permissions import IsClinicStaff
 from django_filters.rest_framework import DjangoFilterBackend
 from attention.filters import RecordFilterSet
 
+
 class RecordViewSet(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
@@ -35,9 +38,8 @@ class RecordViewSet(
     """ Record viewset """
     queryset = Record.objects.all()
     serializer_class = RecordModelSerializer
-    filter_backends = [DjangoFilterBackend]  
+    filter_backends = [DjangoFilterBackend]
     filterset_class = RecordFilterSet
-
 
     def get_permissions(self):
         permissions = [IsAuthenticated]
@@ -47,6 +49,38 @@ class RecordViewSet(
             permissions += [IsClinicStaff]
 
         return [p() for p in permissions]
+
+    @action(detail=False, methods=['get'])
+    def attention_types(self, request, *args, **kwargs):
+        data = {}
+        values = Record.ATTENTION_TYPES
+        for value in values:
+            data[value[0]] = value[1]
+        return Response(data)
+
+    @action(detail=False, methods=['get'])
+    def attendance_states(self, request, *args, **kwargs):
+        data = {}
+        values = Record.ATTENDANCE_STATES
+        for value in values:
+            data[value[0]] = value[1]
+        return Response(data)
+
+    @action(detail=False, methods=['get'])
+    def voucher_states(self, request, *args, **kwargs):
+        data = {}
+        values = Record.VOUCHER_STATES
+        for value in values:
+            data[value[0]] = value[1]
+        return Response(data)
+
+    @action(detail=False, methods=['get'])
+    def surfaces(self, request, *args, **kwargs):
+        data = {}
+        values = Record.SURFACES
+        for value in values:
+            data[value[0]] = value[1]
+        return Response(data)
 
 
 class NoAttendedRecordViewSet(
